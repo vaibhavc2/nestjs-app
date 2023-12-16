c5e04a4160053cb08415c1a6dd474f1c1dc70c55
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-12-16 22:17:27 +0530

    extend simple functionality

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from "@nestjs/common";
 import { UserDTO } from "./dto/user.dto";
 
 const USERS = [] as UserDTO[];
@@ -21,5 +29,21 @@ export class UsersController {
   @Get("/:id")
   getUser(@Param("id") id: string) {
     return USERS.find((user) => user.id === id);
+  }
+
+  @Put("/:id")
+  updateUser(@Param("id") id: string, @Body() user: UserDTO) {
+    const userIndex = USERS.findIndex((user) => user.id === id);
+    if (!userIndex) return "User not found!";
+    else USERS[userIndex] = user;
+    return "User updated successfully!";
+  }
+
+  @Delete("/:id")
+  deleteUser(@Param("id") id: string) {
+    const userIndex = USERS.findIndex((user) => user.id === id);
+    if (!userIndex) return "User not found!";
+    else USERS.splice(userIndex, 1);
+    return "User deleted successfully!";
   }
 }
